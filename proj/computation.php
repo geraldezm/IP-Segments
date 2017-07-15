b977ca43538c3d51f56934dd43195d8cffb0b78e
Maureen Geraldez (DCS-PH-INTRN) <maureeng@ph.trendnet.org>
2017-07-15 15:32:41 +0800

    added computation.php and elastic.php

--- proj/computation.php
+++ proj/computation.php
@@ -1,22 +1,46 @@
-<html>
-   <body>
-      
-      <?php
-         $ip = "192.168.10-25";
-         echo $ip;
-         echo "</br>";
+    <html>
+       <body>
 
-        $ipadd = explode(".", $ip);
+          <?php
+          
+          funcion ipRange($ip){
+            $ipArray = []
 
-        for ($i = 0; $i < sizeof($ipadd); $i++){
-            #echo $i;
-            #echo strpos($ipadd[$i], "-");
-	        if(strpos(($ipadd[$i]), "-") == true) {
-		        echo "There is a '-' on the ". ($i + 1) . " octet";
-    	    }
+            $ipadd = explode(".", $ip);
 
-        }
-      ?>
-      
-   </body>
-</html>
+            for ($i = 0; $i < sizeof($ipadd); $i++){
+                if(strpos(($ipadd[$i]), "-") == true) {
+                    $octet = $i;
+                    $iprange = explode("-",$ipadd[$i]);
+                    $min = $iprange[0];
+                    $max = $iprange[1];    
+                }
+
+            }
+
+
+            for($i = $min; $i <= $max; $i++){
+                $ipadd[$octet] = $i;
+               
+                for ($j = 0; $j < sizeof($ipadd); $j++){
+
+                    $address = implode(".", $ipadd);
+                }
+                
+                $ipArray[] = $address;
+               
+            }
+              
+            return $ipArray;
+           
+            /*foreach ($ipArray as $oneIP){
+                #echo "$oneIP</br>";
+            }*/
+
+        } 
+            
+
+          ?>
+
+       </body>
+    </html>
